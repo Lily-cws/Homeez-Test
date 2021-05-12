672b7ca2a08537a90693551d9d0eecf882acf7d2
lilycws <lilymary55@hotmail.com>
2021-05-13 00:09:02 +0800

    fourth commit : Change api port and database connection setting to AWS

--- database/database.js
+++ database/database.js
@@ -1,11 +1,20 @@
 const Pool = require('pg').Pool;
 
+//local
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "Pass1234",
+//     host: "localhost",
+//     port: 5432,
+//     database: "homeez"
+// });
+
 const pool = new Pool({
     user: "postgres",
-    password: "Pass1234",
-    host: "localhost",
+    password: "pass1234",
+    host: "database-homeez.cxal7ebvyqej.ap-southeast-1.rds.amazonaws.com",
     port: 5432,
-    database: "homeez"
+    database: "dbhomeez"
 });
 
 module.exports = pool;
--- nodejs/server.js
+++ nodejs/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 8080;
 const hostname = "localhost";
 const cors = require("cors");
 const pool = require("../database/database");
